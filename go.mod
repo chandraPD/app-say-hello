658fd33ec8e49ae6a53e0b8d76254689f91125ad
chandraPD <chandraputradinata@gmail.com>
2022-02-13 13:36:14 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/chandraPD/app-say-hello
+
+go 1.17
+
+require github.com/chandraPD/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package app_say_hello
+
+import (
+	"fmt"
+	go_say_hello "github.com/chandraPD/go-say-hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
